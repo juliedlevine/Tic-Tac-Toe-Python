942d61988d21b9e3bcea07e5676bcfa5046a861a
juliemdyer <juliemdyer@gmail.com>
2017-02-09 19:16:38 -0500

    Added comments to Tic Tac Toe game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,12 +16,15 @@ def draw_board():
     print " --- --- --- "
     print "| %s | %s | %s |" % (square_7, square_8, square_9)
     print " --- --- --- "
-draw_board()
 
+# Initialize game, draw board with squares, set play count to 9
+draw_board()
 print "Let's play Tic Tac Toe!"
 play_count = 9
 
+# Start game
 while play_count > 0:
+    # Check for a winner after every cycle
     def check_winner():
         if (square_1 == "x" and square_2 == "x" and square_3 == "x") or (square_4 == "x" and square_5 == "x" and square_6 == "x") or (square_7 == "x" and square_8 == "x" and square_9 == "x") or (square_1 == "x" and square_4 == "x" and square_7 == "x") or (square_3 == "x" and square_6 == "x" and square_9 == "x") or (square_1 == "x" and square_5 == "x" and square_9 == "x") or (square_3 == "x" and square_5 == "x" and square_7 == "x"):
             print "Player 1 wins!"
@@ -32,11 +35,14 @@ while play_count > 0:
             global play_count
             play_count = 0
 
+    # If play_count is even, it's player 1's turn
     if play_count %2 != 0:
         try:
             player_1 = int(raw_input("Player 1, enter a number: "))
+        # If player 2 enters a non-number, ask them to try again
         except ValueError:
             player_1 = int(raw_input("That's not a number, enter a number: "))
+        # Loop through each number option, if it matches the square, change the square to an x
         for i in range(1, 10):
             if player_1 == 1:
                 square_1 = "x"
@@ -65,15 +71,19 @@ while play_count > 0:
             elif player_1 == 9:
                 square_9 = "x"
                 break
+        # Decrease play count, check for a winner and draw the updated board
         play_count -= 1
         check_winner()
         draw_board()
 
+    # If play_count is odd, it's player 2's turn
     elif play_count %2 == 0:
         try:
             player_2 = int(raw_input("Player 2, enter a number: "))
+        # If player 2 enters a non-number, ask them to try again
         except ValueError:
             player_2 = int(raw_input("That's not a number, enter a number: "))
+        # Loop through each number option, if it matches the square, change the square to an o
         for i in range(1, 10):
             if player_2 == 1:
                 square_1 = "o"
@@ -102,9 +112,11 @@ while play_count > 0:
             elif player_2 == 9:
                 square_9 = "o"
                 break
+        # Decrease play count, check for a winner and draw the updated board
         play_count -= 1
         check_winner()
         draw_board()
 
+    # Check for stalemate - if play count gets to 0 and there hasn't been a winner, stalemate
     if play_count == 0:
         print "Sorry stalemate! No one won."
